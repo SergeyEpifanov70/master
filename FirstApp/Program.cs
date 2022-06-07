d75b76e06be8c35829de93ce76bd042aeee625d5
Сергей Епифанов <5438591@mail.ru>
2022-06-07 12:43:59 +0300

    Задание 3.2.4 - Создание нового проектапо основам C# с первым файлом

---
+++ FirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		string myName1;
+		myName1 = "Jane";
+
+		Console.WriteLine(myName1);
+
+		Console.ReadKey();
+	}
+}
