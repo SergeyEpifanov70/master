73b7dd1bdc6cb5cff368cfecefcfaa3df287846b
Сергей Епифанов <5438591@mail.ru>
2022-06-08 10:54:42 +0300

    Решение задачи 3.3.2

---
+++ Task-3_3_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		string MyName = "Sergey";
+		byte MyAge = 51;
+		bool HaveAnimal = false;
+		double MySizeShoes = 43.5;
+
+		Console.WriteLine("Меня зовут - " + MyName);
+		Console.WriteLine("Мой возраст - " + MyAge);
+		Console.WriteLine("Наличие домашнего животного - " + HaveAnimal);
+		Console.WriteLine("Мой размер обуви - " + MySizeShoes);
+
+	}
+}
